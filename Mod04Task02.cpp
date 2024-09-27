feb045bb32d7afd3e545f88ca07f0a7854a80a52
Maxim Kocherga <mkocherga@mail.ru>
2024-09-27 22:54:13 +0300

    Initial commit

---
+++ Mod04Task02.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+// Задача 2. Счета
+
+struct bankAccount
+{
+    int accountNum;
+    std::string name;
+    int accountSum;
+};
+
+void setNewSum(bankAccount* sum)
+{
+    int newSum;
+    std::cout << "Please set a new sum:" << std::endl;
+    std::cin >> newSum;
+    sum->accountSum = newSum;
+}
+int main()
+{
+    bankAccount person;
+    std::cout << "Please enter an account number:" << std::endl;
+    std::cin >> person.accountNum;
+
+    std::cout << "Please enter a name:" << std::endl;
+    std::cin >> person.name;
+
+    std::cout << "Please enter a sum:" << std::endl;
+    std::cin >> person.accountSum;
+        
+    std::cout << "Accont number: " << person.accountNum << std::endl;
+    std::cout << "Accont name: " << person.name << std::endl;
+    std::cout << "Accont sum: " << person.accountSum << std::endl;
+
+    setNewSum(&person);
+
+    std::cout << person.accountNum << "\t" << person.name << "\t" << person.accountSum << std::endl;
+    
+return 0;
+}
